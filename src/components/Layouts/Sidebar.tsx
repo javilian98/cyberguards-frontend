3b5f8e66dc7921db64b17884fe28abe5658a4132
javilian98 <javierlian98@gmail.com>
2024-01-29 13:17:06 +0800

    [Javier] feat: styling avatar fallback bg/text color

--- src/components/Layouts/Sidebar.tsx
+++ src/components/Layouts/Sidebar.tsx
@@ -1,4 +1,3 @@
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -10,11 +9,7 @@ import {
   FiFolderMinus,
 } from "react-icons/fi";
 
-interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  //   playlists: Playlist[]
-}
-
-export default function Sidebar({ className }: SidebarProps) {
+export default function Sidebar() {
   return (
     <div>
       <div className="space-y-4 py-4 border-r-2 min-h-dvh w-[300px] flex flex-col justify-between">
@@ -28,9 +23,6 @@ export default function Sidebar({ className }: SidebarProps) {
             </div>
           </div>
           <div className="px-3 py-2">
-            {/* <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
-            Library
-          </h2> */}
             <div className="space-y-1">
               <Button variant="secondary" className="w-full justify-start">
                 <FiHome className="h-5 w-5 mr-3" />
@@ -55,25 +47,11 @@ export default function Sidebar({ className }: SidebarProps) {
         <div className="px-4">
           <Separator className="mb-4" />
           <Button variant="ghost" className="w-full justify-start">
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-4 w-4"
-            >
-              <path d="M21 15V6" />
-              <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-              <path d="M12 12H3" />
-              <path d="M16 6H3" />
-              <path d="M12 18H3" />
-            </svg> */}
             <Avatar className="w-10 h-10 bg-gray-600 mr-3 rounded-full">
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
+              <AvatarImage src="https://github.com/shadcn.pnga" />
+              <AvatarFallback className="bg-blue-600 text-white">
+                JD
+              </AvatarFallback>
             </Avatar>
             John Doe
           </Button>
