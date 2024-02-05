e94927d730823c441449284a38e1baf41fa424d2
javilian98 <javierlian98@gmail.com>
2024-02-05 17:06:20 +0800

    [Javier] feat: added ComboBox & bottom bar cancel and create/save buttons

--- src/pages/CreateEditCase.tsx
+++ src/pages/CreateEditCase.tsx
@@ -3,7 +3,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -21,15 +20,16 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import RiskScoreSlider from "@/components/RiskScoreSlider/RiskScoreSlider";
 
-import { LuImport } from "react-icons/lu";
+import { LuImport, LuPlus, LuTrash2 } from "react-icons/lu";
+import { ComboBox } from "@/components/ComboBox/ComboBox";
 
 function CreateEditCase() {
   return (
-    <>
+    <div className="relative">
       <h1 className="text-2xl font-semibold tracking-tight mb-10">
         Create a Case
       </h1>
-      <Card className="max-w-[700px]">
+      <Card className="max-w-[700px] mb-14">
         <CardHeader>
           <CardTitle className="flex items-center justify-between">
             Case Details
@@ -49,11 +49,14 @@ function CreateEditCase() {
             <Input type="text" placeholder="Enter your Case Title" />
           </div>
           <div className="grid w-full items-center gap-2 mt-6">
-            <Label htmlFor="message">Your message</Label>
-            <Textarea placeholder="Type your message here." id="message" />
+            <Label htmlFor="case-description">Case Description</Label>
+            <Textarea
+              placeholder="Type your message here."
+              id="case-description"
+            />
           </div>
           <div className="grid w-full max-w-sm items-center gap-2 mt-6">
-            <Label htmlFor="email">Risk Status</Label>
+            <Label htmlFor="risk-status">Risk Status</Label>
             <Select>
               <SelectTrigger className="w-[200px]">
                 <SelectValue placeholder="Select Risk Status" />
@@ -72,7 +75,7 @@ function CreateEditCase() {
             <RiskScoreSlider defaultValue={[0]} />
           </div>
           <div className="grid w-full max-w-sm items-center gap-2 mt-8">
-            <Label htmlFor="email">Suspect Type</Label>
+            <Label htmlFor="suspect-type">Suspect Type</Label>
             <Select>
               <SelectTrigger className="w-[250px]">
                 <SelectValue placeholder="Select Suspect Type" />
@@ -90,12 +93,31 @@ function CreateEditCase() {
               </SelectContent>
             </Select>
           </div>
+
+          <div className="grid w-full max-w-sm items-center gap-2 mt-8">
+            <Label htmlFor="email">Assignee</Label>
+            <p className="text-sm text-muted-foreground">
+              Select an analyst to be in charge of this case.
+            </p>
+            <ComboBox />
+          </div>
+          <div className="grid w-full max-w-sm items-center gap-2 mt-6">
+            <Label htmlFor="case-title">Threat Page Location</Label>
+            <Input type="text" placeholder="Page URL" />
+          </div>
         </CardContent>
-        {/* <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter> */}
       </Card>
-    </>
+      <div className="fixed bottom-0 flex justify-center gap-4 w-[calc(100%-300px)] py-2 px-3 bg-white border-t-2">
+        <Button variant="outline" className="w-[150px]">
+          <LuTrash2 className="w-5 h-5 mr-2" />
+          Discard
+        </Button>
+        <Button className="w-[150px]">
+          <LuPlus className="w-5 h-5 mr-2" />
+          Create Case
+        </Button>
+      </div>
+    </div>
   );
 }
 
