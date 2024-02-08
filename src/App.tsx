b7dc86c4a898dad04440d3b0704d7d47f4d9dabd
Haiyang <60722490+EasonXu-HY99@users.noreply.github.com>
2024-02-08 13:45:38 +0800

    [Haiyang] feat: Set Sign page as default route and update navigation
    - Change default home page route from "/" to "/home" and adjust Sidebar to appear conditionally, excluding the Sign page.
    - Add new UI components: Card, Input, Label, Tabs, and Toast to enhance functionality.
    - Update App.tsx to set Sign page as the initial page for improved user flow.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import Sidebar from "@/components/Layouts/Sidebar/Sidebar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "@/pages/Home";
 import Threats from "@/pages/Threats";
 import Users from "@/pages/Users";
 import Cases from "@/pages/Cases";
+import Sign from "@/pages/Sign";
 
 function App() {
+  const location = useLocation(); // This hook gives you access to the location object
+  const showSidebar = location.pathname !== '/'; // Check if we're not on the sign-in page (which is the root)
   return (
     <>
-      <div className="flex">
-        <Sidebar />
-        <div className="py-4 px-3">
+      <div className={`flex ${!showSidebar && 'justify-center'}`}> {/* Center the content if there's no sidebar */}
+        {showSidebar && <Sidebar />} {/* Render the Sidebar only if showSidebar is true */}
+        <div className="py-4 px-3 w-full">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Sign />} />
+            {/* Other routes remain the same */}
+            <Route path="/home" element={<Home />} />
             <Route path="/threats" element={<Threats />} />
             <Route path="/users" element={<Users />} />
             <Route path="/cases" element={<Cases />} />
