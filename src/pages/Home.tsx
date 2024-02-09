2e86c629937fca37d1b3d7c3532bef4bddb39127
javilian98 <javierlian98@gmail.com>
2024-02-09 10:53:36 +0800

    [Javier] ci: removed unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,15 @@
-import { cn } from "@/lib/utils"
-import { Link } from 'react-router-dom';
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { cn } from "@/lib/utils";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 import { ArrowRightIcon } from "lucide-react";
 
 const latestThreats = [
@@ -44,7 +38,7 @@ const latestThreats = [
     threatsCategory: "Early warning",
     threatsDate: "29 Feb 2024",
   },
-]
+];
 
 const riskyUsers = [
   {
@@ -72,10 +66,9 @@ const riskyUsers = [
     riskScore: "4",
     latestRisk: "0",
   },
-]
+];
 
-
-type CardProps = React.ComponentProps<typeof Card>
+type CardProps = React.ComponentProps<typeof Card>;
 
 function Home({ className, ...props }: CardProps) {
   return (
@@ -86,31 +79,39 @@ function Home({ className, ...props }: CardProps) {
         </CardHeader>
         <CardContent className="grid gap-4">
           {/* Your other card content here */}
-          
+
           {/* Table Integration */}
           <Table>
             <TableHeader>
               <TableRow>
                 <TableHead className="w-[225px]">Title</TableHead>
-                <TableHead className="w-[135px]" >Category</TableHead>
+                <TableHead className="w-[135px]">Category</TableHead>
                 <TableHead>Date</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
               {latestThreats.map((threatsTitle) => (
                 <TableRow key={threatsTitle.threatsTitle}>
-                  <TableCell className="font-small">{threatsTitle.threatsTitle}</TableCell>
-                  <TableCell className="font-small">{threatsTitle.threatsCategory}</TableCell>
-                  <TableCell className="font-small">{threatsTitle.threatsDate}</TableCell>
+                  <TableCell className="font-small">
+                    {threatsTitle.threatsTitle}
+                  </TableCell>
+                  <TableCell className="font-small">
+                    {threatsTitle.threatsCategory}
+                  </TableCell>
+                  <TableCell className="font-small">
+                    {threatsTitle.threatsDate}
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
           <Link to="/Threats">
-            <Button variant="link">View all<ArrowRightIcon /></Button>
+            <Button variant="link">
+              View all
+              <ArrowRightIcon />
+            </Button>
           </Link>
         </CardContent>
-        
       </Card>
       <Card className={cn("w-[520px]", className)} {...props}>
         <CardHeader>
@@ -118,7 +119,7 @@ function Home({ className, ...props }: CardProps) {
         </CardHeader>
         <CardContent className="grid gap-4">
           {/* Your other card content here */}
-          
+
           {/* Table Integration */}
           <Table>
             <TableHeader>
@@ -131,24 +132,29 @@ function Home({ className, ...props }: CardProps) {
             <TableBody>
               {riskyUsers.map((riskyName) => (
                 <TableRow key={riskyName.riskyName}>
-                  <TableCell className="font-small">{riskyName.riskyName}</TableCell>
-                  <TableCell className="font-small">{riskyName.riskScore}</TableCell>
-                  <TableCell className="font-small">{riskyName.latestRisk}</TableCell>
+                  <TableCell className="font-small">
+                    {riskyName.riskyName}
+                  </TableCell>
+                  <TableCell className="font-small">
+                    {riskyName.riskScore}
+                  </TableCell>
+                  <TableCell className="font-small">
+                    {riskyName.latestRisk}
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
           <Link to="/Users">
-            <Button variant="link">View all<ArrowRightIcon /></Button>
+            <Button variant="link">
+              View all
+              <ArrowRightIcon />
+            </Button>
           </Link>
         </CardContent>
-        
       </Card>
-
     </div>
-  )
-  
+  );
 }
-
 
 export default Home;
