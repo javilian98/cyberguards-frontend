18c2354a5fb6ffe96e51061c5f1448b18fd281c0
javilian98 <javierlian98@gmail.com>
2024-02-09 10:30:31 +0800

    [Javier] feat: added AlertDialog and show when delete action button is clicked, added delete functionality frontend logic

---
+++ src/components/DataTable/Cases/CasesColumns.tsx
@@ -0,0 +1,159 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useAlertDialogStore } from "@/stores/useAlertDialogStore";
+import { useCaseStore } from "@/stores/useCaseStore";
+import { Column, ColumnDef } from "@tanstack/react-table";
+import {
+  LuArrowUpDown,
+  LuEye,
+  LuMoreHorizontal,
+  LuPencilLine,
+  LuTrash2,
+} from "react-icons/lu";
+import { Case } from "@/types/types";
+
+const renderSortButton = (column: Column<Case>, columnName: string) => {
+  return (
+    <Button
+      variant="ghost"
+      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    >
+      {columnName}
+      <LuArrowUpDown className="ml-2 h-4 w-4" />
+    </Button>
+  );
+};
+
+export const casesColumns: ColumnDef<Case>[] = [
+  {
+    id: "select",
+    header: ({ table }) => {
+      const { cases, setSelectedCases } = useCaseStore.getState();
+
+      return (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => {
+            table.toggleAllPageRowsSelected(!!value);
+
+            if (table.getIsAllPageRowsSelected()) {
+              setSelectedCases([]);
+            } else {
+              setSelectedCases(cases);
+            }
+          }}
+          aria-label="Select all"
+        />
+      );
+    },
+    cell: ({ row }) => {
+      const { selectedCases, setSelectedCases } = useCaseStore.getState();
+
+      return (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => {
+            row.toggleSelected(!!value);
+
+            if (row.getIsSelected()) {
+              const newSelectedCases = selectedCases.filter(
+                (item) => item.id !== row.original.id
+              );
+              setSelectedCases(newSelectedCases);
+            } else {
+              setSelectedCases([...selectedCases, row.original]);
+            }
+          }}
+          aria-label="Select row"
+        />
+      );
+    },
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    header: "Case ID",
+  },
+  {
+    accessorKey: "title",
+    header: ({ column }) => renderSortButton(column, "Case Title"),
+  },
+  {
+    accessorKey: "riskStatus",
+    header: ({ column }) => renderSortButton(column, "Risk Status"),
+  },
+  {
+    accessorKey: "riskScore",
+    header: ({ column }) => renderSortButton(column, "Risk Score"),
+  },
+  {
+    accessorKey: "caseDateTime",
+    header: ({ column }) => renderSortButton(column, "Case Date Time"),
+  },
+  {
+    accessorKey: "assignee",
+    header: ({ column }) => renderSortButton(column, "Assignee"),
+  },
+  // {
+  //   accessorKey: "assignedDateTime",
+  //   header: ({ column }) => renderSortButton(column, "Assigned Date Time"),
+  // },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { currentSelectedCase, setCurrentSelectedCase } =
+        useCaseStore.getState();
+
+      const { setSingleRowActionDialogOpen } = useAlertDialogStore.getState();
+
+      const handleDeleteDialogVisibility = () => {
+        setCurrentSelectedCase(row.original);
+        setSingleRowActionDialogOpen(true);
+
+        console.log(currentSelectedCase);
+      };
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <LuMoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <LuEye className="w-4 h-4 mr-2" />
+              View Case
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <LuPencilLine className="w-4 h-4 mr-2" />
+              Edit Case
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="text-red-600"
+              onClick={handleDeleteDialogVisibility}
+            >
+              <LuTrash2 className="w-4 h-4 mr-2" />
+              Delete Case
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/pages/Cases.tsx
+++ src/pages/Cases.tsx
@@ -1,266 +1,69 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useCaseStore } from "@/stores/useCaseStore";
+
 import { DataTable } from "@/components/DataTable/DataTable";
 import Search from "@/components/Search/Search";
-
-import { Checkbox } from "@/components/ui/checkbox";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-import { Column, ColumnDef } from "@tanstack/react-table";
-
-import { LuArrowUpDown, LuMoreHorizontal, LuPlus } from "react-icons/lu";
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import FixedHeader from "@/components/Layouts/Header/FixedHeader";
-import { Link } from "react-router-dom";
+import { useAlertDialogStore } from "@/stores/useAlertDialogStore";
+import { casesColumns } from "@/components/DataTable/Cases/CasesColumns";
+import { casesData } from "@/components/DataTable/Cases/casesData";
 
-interface Case {
-  id: string;
-  title: string;
-  riskStatus: "low" | "medium" | "high";
-  riskScore: number;
-  caseDateTime: string;
-  assignee: string;
-  assignedDateTime: string;
-}
-
-const allCases: Case[] = [
-  {
-    id: "728ed52f",
-    title: "Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 72,
-    caseDateTime: "21 Jan 2024, 2:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "21 Jan 2024, 3:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 69,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 8,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 62,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 32,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 29,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 3,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 14,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 85,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 33,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 42,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 23,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-];
-
-const myCases: Case[] = [
-  {
-    id: "728ed52f",
-    title: "Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 72,
-    caseDateTime: "21 Jan 2024, 2:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "21 Jan 2024, 3:00pm",
-  },
-  {
-    id: "728ed52g",
-    title: "The Potential Threat From Frequent “After Hour Login",
-    riskStatus: "high",
-    riskScore: 69,
-    caseDateTime: "23 Jan 2024, 8:30pm",
-    assignee: "Chris Tan",
-    assignedDateTime: "25 Jan 2024, 5:00pm",
-  },
-];
-
-const columns: ColumnDef<Case>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "id",
-    header: "Case ID",
-  },
-  {
-    accessorKey: "title",
-    header: ({ column }) => renderSortButton(column, "Case Title"),
-  },
-  {
-    accessorKey: "riskStatus",
-    header: ({ column }) => renderSortButton(column, "Risk Status"),
-  },
-  {
-    accessorKey: "riskScore",
-    header: ({ column }) => renderSortButton(column, "Risk Score"),
-  },
-  {
-    accessorKey: "caseDateTime",
-    header: ({ column }) => renderSortButton(column, "Case Date Time"),
-  },
-  {
-    accessorKey: "assignee",
-    header: ({ column }) => renderSortButton(column, "Assignee"),
-  },
-  {
-    accessorKey: "assignedDateTime",
-    header: ({ column }) => renderSortButton(column, "Assigned Date Time"),
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => {
-      const payment = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <LuMoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
-
-const renderSortButton = (column: Column<Case>, columnName: string) => {
-  return (
-    <Button
-      variant="ghost"
-      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-    >
-      {columnName}
-      <LuArrowUpDown className="ml-2 h-4 w-4" />
-    </Button>
-  );
-};
+import { LuPlus } from "react-icons/lu";
 
 function Cases() {
+  const cases = useCaseStore((state) => state.cases);
+  const setCases = useCaseStore((state) => state.setCases);
+  const selectedCases = useCaseStore((state) => state.selectedCases);
+  const setSelectedCases = useCaseStore((state) => state.setSelectedCases);
+  const currentSelectedCase = useCaseStore(
+    (state) => state.currentSelectedCase
+  );
+
+  const isSingleRowActionDialogOpen = useAlertDialogStore(
+    (state) => state.isSingleRowActionDialogOpen
+  );
+  const setSingleRowActionDialogOpen = useAlertDialogStore(
+    (state) => state.setSingleRowActionDialogOpen
+  );
+
+  const handleDelete = () => {
+    const newCases = cases.filter((item) => item.id !== currentSelectedCase.id);
+    setCases(newCases);
+  };
+
+  const handleDeleteAllSelected = () => {
+    const newCases = cases.filter(
+      (item) =>
+        !selectedCases.some((selectedCase) => selectedCase.id === item.id)
+    );
+    setCases(newCases);
+    setSelectedCases([]);
+  };
+
+  useEffect(() => {
+    setCases(casesData);
+  }, [setCases]);
+
   return (
     <>
       <FixedHeader>
-        <Search placeholderText="Search for a case" drawerTitle="Filter Cases">
+        <Search placeholderText="Search case title" drawerTitle="Filter Cases">
           asd
         </Search>
+
         <Link to="/cases/create">
           <Button>
             <LuPlus className="w-5 h-5 mr-2 text-white" />
@@ -268,6 +71,7 @@ function Cases() {
           </Button>
         </Link>
       </FixedHeader>
+
       <Tabs defaultValue="all cases" className="mt-20">
         <TabsList>
           <TabsTrigger value="all cases" className="w-[200px]">
@@ -277,13 +81,46 @@ function Cases() {
             My Cases
           </TabsTrigger>
         </TabsList>
+
         <TabsContent value="all cases" className="mt-4">
-          <DataTable columns={columns} data={allCases} />
+          <DataTable
+            columns={casesColumns}
+            data={cases}
+            actionDelete={handleDeleteAllSelected}
+          />
         </TabsContent>
         <TabsContent value="my cases">
-          <DataTable columns={columns} data={myCases} />
+          <DataTable
+            columns={casesColumns}
+            data={cases.filter((item) => item.assignee === "John Doe")}
+          />
         </TabsContent>
       </Tabs>
+
+      <AlertDialog
+        open={isSingleRowActionDialogOpen}
+        onOpenChange={() => setSingleRowActionDialogOpen(false)}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              Are you sure you want to delete this case?
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete the
+              case.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              onClick={() => setSingleRowActionDialogOpen(false)}
+            >
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleDelete}>Delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 }
