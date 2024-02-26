0eac294c9b793612aa08f6a5693062f82be973e5
javilian98 <javierlian98@gmail.com>
2024-02-26 16:47:35 +0800

    [Javier] refactor: changed how form data format is submitted

--- src/pages/CreateEditCase.tsx
+++ src/pages/CreateEditCase.tsx
@@ -18,6 +18,7 @@ import {
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -38,10 +39,32 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import RiskScoreSlider from "@/components/RiskScoreSlider/RiskScoreSlider";
 
-import { LuCheckCircle, LuImport, LuPlus, LuTrash2 } from "react-icons/lu";
-import { ComboBox } from "@/components/ComboBox/ComboBox";
+import {
+  LuAlertCircle,
+  LuCheck,
+  LuCheckCircle,
+  LuChevronsUpDown,
+  LuImport,
+  LuPlus,
+  LuTrash2,
+} from "react-icons/lu";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { createCase, getCase, updateCase } from "@/api/casesApi";
+import { getUserList } from "@/api/usersApi";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -50,7 +73,15 @@ import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { useAlertDialogStore } from "@/stores/useAlertDialogStore";
 import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { UserListItem } from "@/types/types";
 
+const assigneeSchema = z
+  .object({
+    id: z.string().nullable(),
+    fullName: z.string().nullable(),
+  })
+  .optional();
 const formSchema = z.object({
   title: z.string().min(2, {
     message: "Title must be at least 2 characters.",
@@ -62,6 +93,13 @@ const formSchema = z.object({
     required_error: "Risk Status must be selected.",
   }),
   riskScore: z.array(z.number()).length(1),
+  // assignee: z
+  //   .string()
+  //   .min(2, {
+  //     message: "You must select an assignee",
+  //   })
+  //   .optional(),
+  assignee: z.union([assigneeSchema, z.undefined()]).optional(),
   // suspectType: z.number(),
   threatPageUrl: z.string().url(),
 });
@@ -79,6 +117,8 @@ function CreateEditCase() {
     (state) => state.setSingleRowActionDialogOpen
   );
 
+  const [isFormEdited, setFormEdited] = useState(false);
+
   const {
     data: caseDetailData,
     error: caseDetailError,
@@ -87,6 +127,8 @@ function CreateEditCase() {
     queryKey: ["cases", id],
     queryFn: async () => {
       const data = await getCase(id as string);
+
+      console.log("data ", data);
 
       // Object.keys(data).forEach((key) => {
       //   console.log("key", key);
@@ -97,6 +139,10 @@ function CreateEditCase() {
       form.setValue("riskStatus", data.riskStatus);
       form.setValue("riskScore", [data.riskScore]);
       form.setValue("threatPageUrl", data.threatPageUrl);
+      form.setValue("assignee", {
+        id: data.assigneeId ?? null,
+        fullName: data.assignee?.fullName ?? null,
+      });
 
       return data;
     },
@@ -105,8 +151,21 @@ function CreateEditCase() {
 
   const createCaseMutation = useMutation({
     mutationKey: ["cases"],
-    mutationFn: (caseItem: z.infer<typeof formSchema>) =>
-      createCase({ ...caseItem, riskScore: caseItem.riskScore[0] }),
+    mutationFn: async (caseItem: z.infer<typeof formSchema>) => {
+      const assigneeFormValue = form.getValues().assignee;
+
+      const assigneeFound = assigneeListData?.find((assignee) => {
+        const fullName = assignee.firstName + " " + assignee.lastName;
+
+        return fullName === assigneeFormValue?.fullName;
+      });
+
+      return await createCase({
+        ...caseItem,
+        riskScore: caseItem.riskScore[0],
+        assigneeId: assigneeFound?.id,
+      });
+    },
     onError: () => {},
     onSuccess: async (data) => {
       await queryClient.invalidateQueries({ queryKey: ["cases"] });
@@ -119,11 +178,26 @@ function CreateEditCase() {
 
   const updateCaseMutation = useMutation({
     mutationKey: ["updatecase", id],
-    mutationFn: (caseItem: z.infer<typeof formSchema>) =>
-      updateCase(
-        { ...caseItem, riskScore: caseItem.riskScore[0] },
+    mutationFn: async (caseItem: z.infer<typeof formSchema>) => {
+      const assigneeFormValue = form.getValues().assignee;
+
+      const assigneeFound = assigneeListData?.find((assignee) => {
+        const fullName = assignee.firstName + " " + assignee.lastName;
+
+        return fullName === assigneeFormValue?.fullName;
+      });
+
+      console.log("updateCase ", caseItem);
+
+      return await updateCase(
+        {
+          ...caseItem,
+          riskScore: caseItem.riskScore[0],
+          assigneeId: assigneeFound?.id,
+        },
         id as string
-      ),
+      );
+    },
     onError: () => {},
     onSuccess: async (data) => {
       await queryClient.invalidateQueries({ queryKey: ["cases"] });
@@ -134,7 +208,18 @@ function CreateEditCase() {
     },
   });
 
-  const [isFormEdited, setFormEdited] = useState(false);
+  const { data: assigneeListData } = useQuery({
+    queryKey: ["assignees"],
+    queryFn: async () => {
+      // roleId: 1 is analyst
+      const data = await getUserList({ roleId: 1 });
+
+      // setUsers(data);
+      return data;
+    },
+  });
+
+  console.log("assigneeListData ", assigneeListData);
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -144,6 +229,10 @@ function CreateEditCase() {
       description: "",
       riskStatus: undefined,
       riskScore: [0],
+      assignee: {
+        id: "",
+        fullName: "",
+      },
       threatPageUrl: "",
     },
   });
@@ -184,7 +273,7 @@ function CreateEditCase() {
   };
 
   const renderSubmitButton = () => {
-    console.log("caseDetailData ", caseDetailData);
+    // console.log("caseDetailData ", caseDetailData);
 
     if (caseDetailData) {
       return (
@@ -194,15 +283,19 @@ function CreateEditCase() {
         </Button>
       );
     }
-
-    console.log("hereee");
-
     return (
       <Button className="w-[150px]" type="submit">
         <LuPlus className="w-5 h-5 mr-2" />
         Create Case
       </Button>
     );
+  };
+
+  const computeFullName = (user: UserListItem | undefined) => {
+    if (user == undefined) {
+      return "Select Assignee";
+    }
+    return `${user.firstName} ${user.lastName}`;
   };
 
   if (isCaseDetailLoading) {
@@ -331,11 +424,82 @@ function CreateEditCase() {
               </div>
 
               <div className="grid w-full max-w-sm items-center gap-2 mt-8">
-                <Label htmlFor="email">Assignee</Label>
-                <p className="text-sm text-muted-foreground">
-                  Select an analyst to be in charge of this case.
-                </p>
-                <ComboBox />
+                <FormField
+                  control={form.control}
+                  name="assignee"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col">
+                      <FormLabel>Assignee</FormLabel>
+                      <p className="text-sm text-muted-foreground">
+                        Select an analyst to be in charge of this case.
+                      </p>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              disabled={!assigneeListData}
+                              variant="outline"
+                              role="combobox"
+                              className={cn(
+                                "w-[200px] justify-between",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value
+                                ? computeFullName(
+                                    assigneeListData?.find(
+                                      (assignee) =>
+                                        computeFullName(assignee) ===
+                                        field.value?.fullName
+                                    )
+                                  )
+                                : "Select Assignee"}
+                              <LuChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-[200px] p-0">
+                          <Command>
+                            <CommandInput placeholder="Search language..." />
+                            <CommandEmpty>No user found.</CommandEmpty>
+                            <CommandGroup>
+                              {assigneeListData?.map((assignee) => (
+                                <CommandItem
+                                  value={computeFullName(assignee)}
+                                  key={computeFullName(assignee)}
+                                  onSelect={() => {
+                                    form.setValue("assignee", {
+                                      id: assignee.id,
+                                      fullName: computeFullName(assignee),
+                                    });
+                                  }}
+                                >
+                                  <LuCheck
+                                    className={cn(
+                                      "mr-2 h-4 w-4",
+                                      computeFullName(assignee) ===
+                                        field.value?.fullName
+                                        ? "opacity-100"
+                                        : "opacity-0"
+                                    )}
+                                  />
+                                  {computeFullName(assignee)}
+                                </CommandItem>
+                              ))}
+                            </CommandGroup>
+                          </Command>
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
+                      {!assigneeListData && (
+                        <FormDescription className="flex items-center gap-1 text-red-500">
+                          <LuAlertCircle />
+                          Unable to select an assignee at the moment
+                        </FormDescription>
+                      )}
+                    </FormItem>
+                  )}
+                />
               </div>
               <div className="grid w-full max-w-sm items-center gap-2 mt-6">
                 <FormField
