d4cb9f07ee66f74e349253e8e7953f570f3c1795
tanmymegan <79134357+tanmymegan@users.noreply.github.com>
2024-02-26 11:36:07 +0800

    [Megan] test: added test cases for individual page clicked

--- src/components/Layouts/Sidebar.test.tsx
+++ src/components/Layouts/Sidebar.test.tsx
@@ -1,17 +1,40 @@
 import Sidebar from "@/components/Layouts/Sidebar";
 import Home from "@/pages/Home";
 import Threats from "@/pages/Threats";
+import Users from "@/pages/Users";
+import Cases from "@/pages/Cases";
 import { render, screen, userEvent } from "@/utils/test-utils";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 describe("Unit Testing on Sidebar.tsx", () => {
-  it("should navigate to the correct page when link is clicked", async () => {
+  it("should navigate to Home page when Home link is clicked", async () => {
     render(
       <>
         <MemoryRouter initialEntries={["/"]}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/threats" element={<Threats />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/cases" element={<Cases />} />
+          </Routes>
+          <Sidebar />
+        </MemoryRouter>
+      </>
+    );
+
+    userEvent.click(screen.getByRole("link", { name: /home/i }));
+    await screen.findByText("Home");
+  });
+
+  it("should navigate to Threats page when Threats link is clicked", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/threats" element={<Threats />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/cases" element={<Cases />} />
           </Routes>
           <Sidebar />
         </MemoryRouter>
@@ -19,6 +42,44 @@ describe("Unit Testing on Sidebar.tsx", () => {
     );
 
     userEvent.click(screen.getByRole("link", { name: /threats/i }));
-    await screen.findByText("Threats");
+    await screen.findByText("Home");
+  });
+
+  it("should navigate to Users page when Users link is clicked", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/threats" element={<Threats />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/cases" element={<Cases />} />
+          </Routes>
+          <Sidebar />
+        </MemoryRouter>
+      </>
+    );
+
+    userEvent.click(screen.getByRole("link", { name: /users/i }));
+    await screen.findByText("Home");
+  });
+
+  it("should navigate to Cases page when Cases link is clicked", async () => {
+    render(
+      <>
+        <MemoryRouter initialEntries={["/"]}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/threats" element={<Threats />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/cases" element={<Cases />} />
+          </Routes>
+          <Sidebar />
+        </MemoryRouter>
+      </>
+    );
+
+    userEvent.click(screen.getByRole("link", { name: /cases/i }));
+    await screen.findByText("Home");
   });
 });
